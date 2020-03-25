adda78e44b113e96849fc5a81a7a94e25cf42d61
Puneet Behl <behl4485@gmail.com>
2020-03-25 23:11:10 +0530

    Groovy3 Spock

--- src/test/groovy/example/GreetingSpec.groovy
+++ src/test/groovy/example/GreetingSpec.groovy
@@ -1,12 +1,28 @@
 package example
 
+
+import org.codehaus.groovy.ast.builder.AstBuilder
+import org.codehaus.groovy.ast.expr.GStringExpression
+import org.codehaus.groovy.ast.stmt.BlockStatement
+import org.codehaus.groovy.ast.stmt.ReturnStatement
 import spock.lang.Specification
 
 class GreetingSpec extends Specification {
 
-   void "test canary"() {
-       expect:
-       true
-   }
+    void "test canary"() {
+        expect:
+        true
+    }
 
+    void "test GStringExpression"() {
+
+        setup:
+        AstBuilder astBuilder = new AstBuilder()
+        List<BlockStatement> statements = astBuilder.buildFromString('"from ${String s} where s == \"test\""') as List<BlockStatement>
+        BlockStatement firstBlockStatement = statements[0]
+        ReturnStatement returnStatement = (ReturnStatement) firstBlockStatement.statements[0]
+
+        expect:
+        returnStatement.expression
+    }
 }
